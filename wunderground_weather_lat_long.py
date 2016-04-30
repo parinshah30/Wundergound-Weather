0d350469402f18b0c5cdb3bc1c4ae7b571453b54
Parin Shah <parinshah30@gmail.com>
2016-04-29 22:06:16 -0700

    update the def name

--- wunderground_weather_lat_long.py
+++ wunderground_weather_lat_long.py
@@ -1,19 +1,20 @@
-'''
+"""
 This function will give you the current weather condition given a latitude and longitude
 You need to register with wunderground and get a key
-'''
+"""
 from pprint import pprint
 import requests
 
 # WUNDERGROUND_KEY = '<wunderground key>'
 
-def weather_for_lat_long(latitude=37.33426850, longitude=-121.88618876):
-    '''
+
+def current_weather_from_lat_long(latitude=37.33426850, longitude=-121.88618876):
+    """
     This function will give you the current weather condition given a latitude and longitude
     :param latitude: latitude
     :param longitude: longitude
     :return: json with current weather conditions
-    '''
+    """
     url = 'http://api.wunderground.com/api/{key}/conditions/q/{lat},{long}.json'.format(key=WUNDERGROUND_KEY, lat=latitude, long=longitude)
     r = requests.get(url)
     # pprint(r.json()['current_observation']['weather'])
@@ -22,5 +23,5 @@ def weather_for_lat_long(latitude=37.33426850, longitude=-121.88618876):
 if __name__ == '__main__':
     lat = 37.33426850
     long = -121.88618876
-    weather_for_lat_long(lat, long)
+    current_weather_from_lat_long(lat, long)
 
